c80b148f6ce2c72610ba157f1ba63bf2470acbbb
Habib Adeku <habib.adeku@chargepoint.com>
2023-05-09 15:05:20 +0200

    feat(ci-cd): release test

--- packages/auto-pub-pkg/lib/hello-app/HelloApp.tsx
+++ packages/auto-pub-pkg/lib/hello-app/HelloApp.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 export const HelloApp = (props: any) => {
 
-    return <div style={{fontSize:'40px', textAlign: 'center', fontWeight: 'bolder', color: 'cadetblue'}}>Hello Auto Published Lib 1 cicd update 053</div>;
+    return <div style={{fontSize:'40px', textAlign: 'center', fontWeight: 'bolder', color: 'cadetblue'}}>Hello Auto Published Lib 1 cicd update 054</div>;
   
 };
